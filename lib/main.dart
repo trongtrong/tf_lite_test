f7cab6ac878716fe3c35f6960f9bf085abd37d0a
trongtrong <trongnguyenit1312@gmail.com>
2025-05-14 11:37:29 +0700

    Fix: tflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,39 @@
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
-import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
-import 'package:object_detection_mlkit/screens/facedetection.dart';
-import 'package:object_detection_mlkit/screens/object.dart';
+/*
+ * Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-List<CameraDescription> cameras = [];
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:object_detection_mlkit/ui/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  cameras = await availableCameras();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ML_KIT',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 150,
-            ),
-            //show 3 buttons corresponding to 3 value of typeTrain
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        ObjectDetectorView(typeTrain: TypeTrain.int8),
-                  ),
-                );
-              },
-              child: const Text('   int18   '),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        ObjectDetectorView(typeTrain: TypeTrain.float32),
-                  ),
-                );
-              },
-              child: const Text('Float 32'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        ObjectDetectorView(typeTrain: TypeTrain.float16),
-                  ),
-                );
-              },
-              child: const Text(' Float16'),
-            ),
-          ],
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Live Object Detection TFLite',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-    );
-  }
+        home: const HomeView(),
+      );
 }
-
